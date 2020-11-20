33dba16d19eb4f08d2610fd176d6ce5da23f8f00
okayu322434 <va.food.y.owari1006@gmail.com>
2020-11-20 14:04:06 +0900

    readme

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :rooms, only:[:new, :index, :create]
+  resources :rooms
+
+  resources :changes
 
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_180347) do
+ActiveRecord::Schema.define(version: 2020_11_17_001756) do
+
+  create_table "changes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "room_id"
+    t.text "message", null: false
+    t.integer "spot", null: false
+    t.text "comment", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_changes_on_room_id"
+    t.index ["user_id"], name: "index_changes_on_user_id"
+  end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id"
